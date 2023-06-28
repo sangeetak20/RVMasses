7cad5eb11e33337a84ea1d66386781cb52cf19a3
Sangeeta Kumar <iamawesomeskettles@gmail.com>
2023-06-27 18:49:51 -0700

    Upated code from today that has Planet class and multiple modes of plotting

--- RV Masses Code.py
+++ RV Masses Code.py
@@ -15,57 +15,79 @@ import os
 
 
 # In[ ]:
+#planet class that inherits the values from the target class
+#reads in data file about the planet information 
+class Planet(): 
+    def __init__(self, planet_file): 
+        #reading in the file for planets
+        data = pd.read_csv(planet_file, header = 0)
+        no_pl = len(data) - 1
+        PlanetID = data['PlanetID']
+        Mplanet = data['MPlanet'].values
+        Eccentricity = data['Eccentricity'].values
+        Period = data['Period'].values
+        
+        self.no_pl = no_pl
+        self.PlanetID = PlanetID
+        self.Mplanet = Mplanet
+        self.Eccentricity = Eccentricity
+        self.Period = Period
+        
+        
+# In[ ]:
 
 
-file =  open('Example_file.txt', 'r')
-#finding all of the values from the file
-class Target: 
-    def __init__(ID, Mstar, Mplanet, Period, Eccentricity, Prev_rvs, File_with_prev_rvs, No_obs, t_ref, t_obs, \
-                t_end): 
+#opens example file that will show information about system 
+class Target(Planet): 
+    def __init__(self, file, prev_rv_data = None): 
+        #inherits planet class
+        self.Planets = []
+        super().__init__(Planet, self)
+        
         read_file = file.readlines()
-        file = open('Example_file.txt', 'r')
+        #opens the file and reads it in 
+        file = open(file, 'r')
         for line in read_file:
+            #goes through each line and searches for keyword and assigns the value of the keyword to itself
             if line.split(' ')[0] == 'ID:': 
-                ID = int(stringName[len('ID:'):])
-                
+                ID = line.split(' ')[-1]
+            self.ID = ID
+            
             if line.split(' ')[0] == 'Mstar:': 
-                Mstar = int(stringName[len("Mstar:"):])
-                
-            if line.split(' ')[0] == 'Mplanet:': 
-                Mplanet = int(stringName[len("Mplanet:"):])
-                
-            if line.split(' ')[0] == 'period:': 
-                Period = int(stringName[len("period:"): ])
-                
-            if line.split(' ')[0] == 'eccentricity:': 
-                Eccentricity = int(stringName[len("eccentricity:"):])
-                
-            if line.split(' ')[0] == 'No_obs:': 
-                No_obs = int(stringName[len("No_obs:"):])
+                Mstar = line.split(' ')[-1]
+            Mstar.self == Mstar
             
-            if line.split(' ')[0] == 'prev_rvs:': 
-                Prev_rvs = int(stringName[len('prev_rvs:'):])
+        #if user has previous RV data
+        if prev_rv_data != None:
+            #reads in previous rv data and assigns them variables
+            #these will be used to create plots
+            file_prev_rv = pd.read_csv(prev_rv_data, header = 0)
+
+            prev_rv_time = file_prev_rv['time'] 
+            prev_rv_bjd = file_prev_rv['bjd']
+            prev_rv_errvel = file_prev_rv['errvel']
+            prev_rv_mnvel = file_prev_rv['mnvel']
+
+            self. prev_rv_time = prev_rv_time
+            self.prev_rv_bjd = prev_rv_bjd
+            self.prev_rv_errvel = prev_rv_errvel
+            self.prev_rv_mnvel = prev_rv_mnvel
             
-            if line.split(' ')[0] == 'file_with_prev_rvs:': 
-                File_with_prev_rvs = int(stringName[len('file_with_prev_rvs:'):])
-            
-            if line.split(' ')[0] == 't_ref:': 
-                t_ref = int(stringName[len('t_ref:'):])
-                
-            if line.split(' ')[0] == 't_obs:': 
-                t_obs = int(stringName[len('t_obs:'):])
-                
-            if line.split(' ')[0] == 't_end:': 
-                t_end = int(stringName[len('t_end:')])
-                
-                
-        return ID
+        def load_planets(self, planet_file, npl):
+            for i in range(npl):
+                #subset of file
+                subset = 
+                planet = Planet(subset)
+                self.Planets.append(planet)
 
 
 # In[ ]:
 
 
-class RV_obs: 
+#creates the plots and the simulated data points
+class RV_obs(Target): 
+    def __init__(self, Target): 
+        super().__init__(self, Target)
     
     #square root of G
     G_sqrt = 28.4329
@@ -78,24 +100,64 @@ class RV_obs:
 
     #year in seconds
     yr = 3.154*10**7 
-
-    #creating times that we will use
-    def Generate_times(t_obs, t_end, num = No_obs): 
-        times = np.linspace(t_obs, t_end, no_obs)
-        return times
-    #finding the K value    
-    def K_value(times, Mstar, Mplanet, Period, Eccentricity): 
-        K = G_sqrt / (np.sqrt(1 - Eccentricity**2)) * Mplanet / M_J * (Mstar / M_sun)**(-2/3) * (Period / yr)**(-1/3)
-        return K 
-    #Generating RV values    
-    def RV_values(K,times, t_ref, Period ): 
-        RV_values = -K*np.sin(2*np.pi*(times - t_ref)/Period)
-        return RV_values
-    #generating RV vs time plot    
-    def RV_plot(times, RV_values): 
-        plt.figure()
-        plt.scatter(times, RV_values)
-        plt.xlabel('Time (JD)')
-        plt.ylabel('RV')
-        plt.show()
-
+    
+    #generates the times that the user will be observing over
+    #user must enter in the time they are planning on starting to observe and the date they want to end 
+    #they also have to add the number of observations to establish the cadence 
+    #for now we are assuming an equal cadence rather than a random one 
+    def Generate_times(self, t_obs, t_end, No_obs): 
+        times = np.linspace(t_obs, t_end, No_obs)
+        self.times = times
+        return self.times
+    
+    #K value is the semi amplitude of the sin curve that will be generated from the RV data
+    def K_value(self, times): 
+        #takes from the planet class to calculate the K value
+        K = G_sqrt / (np.sqrt(1 - self.Planet.Eccentricity**2)) * self.Planet.Mplanet / M_J * (self.Planet.Mstar / M_sun)**(-2/3) * \
+        (self.Planet.Period / yr)**(-1/3)
+        self.K = K
+        return self.K 
+    
+    #creates the RV values that will be put into the RV plot
+    def sim_RVs(self): 
+        times = self.Generate_times(t_obs, t_end, No_obs)
+        Star = self.Target
+        RVs = []
+        #this is to make sure that we get multiple RV data points for multiple planets for more than one planet
+        #loops through the number of planets and goes into the Planet class to get the data 
+        for planet in self.Planet.PlanetID: 
+            K = K_value(times, Planet, Target)
+            #takes from the planet class and the K_value class to calculate the RV data point
+            RV_pl = -K*np.sin(2*np.pi*(times - t_ref)/self.Planet.Period) + noise*np.random.randn(len(times))
+            #adds RVs for every planet together 
+            RV_values += RV_pl
+            #all RVs is the final RV
+            self.RV_values = RV_values
+        return self.RV_values
+    
+    #plots the RV data created from the previous function
+    def RV_plot(self, mode = 'sim'): 
+        #simulated data is plotted
+        if mode == 'sim': 
+            fig = plt.figure()
+            plt.scatter(self.times, self.RV_values)
+            plt.xlabel('Time (JD)')
+            plt.ylabel('RV')
+            plt.show()
+        #previous data loaded in from Target class is plotted
+        elif mode == 'prev': 
+            fig = plt.figure()
+            plt.scatter(prev_rv_time.self, self.prev_rv_mnvel)
+            plt.xlabel('Time (JD)')
+            plt.ylabel('RV')
+            plt.show()
+        #both simulated data and previous data are plotted
+        elif mode == 'both': 
+            fig = plt.figure()
+            plt.scatter(prev_rv_time.self, self.prev_rv_mnvel)
+            plt.scatter(self.times, self.RV_values)
+            plt.scatter()
+            plt.xlabel('Time (JD)')
+            plt.ylabel('RV')
+            plt.show()
+        return fig
